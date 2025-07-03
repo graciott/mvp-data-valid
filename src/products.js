645a832b94b7a50ca5476c15cd55de8a30c9ad1f
lucasreis <lucas.graciott@hotmail.com>
2025-07-03 19:12:40 -0300

    Add product list to products.js and update extraction prompt in index.html

---
+++ src/products.js
@@ -0,0 +1,38 @@
+const produtos = `
+Sabão em pó Omo
+Carne de Hambúrguer 
+Gatorade
+Frango 
+Açúcar União
+Desinfetante 
+Petisco Pedigree
+Lasanha Perdigão
+Requeijão Nestlé
+Leite Italac
+Água Sanitária 
+Guaraná Antarctica
+Pepsi
+Pizza 
+Ração Golden
+Desodorante Rexona
+Areia Sanitária Pipicat
+Detergente 
+Creme Dental 
+Ração 
+Papel Higiênico
+Queijo 
+Manteiga 
+Macarrão 
+Esponja 
+Sabonete
+Coca-Cola
+Água 
+Iogurte 
+Arroz 
+Feijão 
+Peixe 
+Shampoo
+Fanta
+Suco Del Valle
+Sal Cisne
+`;
